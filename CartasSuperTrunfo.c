06f188b4080e3a567b00fbb258eff65282743927
Fabio1812pc <fabiolucassilva.fl@gmail.com>
2025-03-14 15:52:27 +0000

    cidade 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,7 +1,49 @@
 #include <stdio.h>
 
 int main () {
-    printf("cartas super trunfo! \n");
+    printf("cidade1! \n");
+    
+    char estado;
+    char codigo[6];
+    char cidade[10];
+    int população;
+    float área;
+    float PIB;
+    int numeropontosturistico;
+
+    printf("Digite o estado: \n");
+    scanf("%s", &estado);
+
+    printf("Digite o codigo: \n");
+    scanf("%s", &codigo);
+
+    printf("digite a cidade: \n");
+    scanf("%s", &cidade);
+
+    printf("digite o numero de população: \s");
+    scanf("%d", &população);
+
+    printf("Tamanho da área: \s");
+    scanf("%f", &área);
+
+    printf("Qual o PIB: \s");
+    scanf("%f",&PIB);
+
+    printf("Numero de pontos turistico: \s");
+    scanf("%d", &numeropontosturistico);
+
+    printf("nome do estado: %s - codigo: %s - cidade:%s", estado, codigo, cidade);
+    printf("população: %d - área: %f - PIB: %f", população, área, PIB);
+    printf("numero pontos turistico: %d");
+
+    return 0;
+
+}
+
+#include <stdio.h>
+
+int main () {
+    printf("cidade2! \n");
     
     char estado;
     char codigo[6];
