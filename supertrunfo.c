e7cce486e12d9d94c458efb39d11e9e10576c730
Fabio1812pc <fabiolucassilva.fl@gmail.com>
2025-03-14 14:19:24 +0000

    nv printf

--- supertrunfo.c
+++ supertrunfo.c
@@ -3,34 +3,27 @@
 int main() {
     printf("cartas super trunfo! \n");
 
-    char estado;
-    char codigoDaCIDADE[6];
-    char cidade;
-    int população;
-    float área;
-    float PIB;
-    int numeroPontosturistico;
+    char estado = "minas gerais";
+    char codigoDaCIDADE[6] = "A 01" ;
+    char cidade[10] = "varginha";
+    int população = 148.800;
+    float área = 395.6;
+    float PIB =  8093809.94;
+    int numeroPontosturistico = 15;
     
-    printf("Digite o estado: \n");
-    scanf("%s", &estado);
+    printf("Digite o estado: \n", estado);
 
-    printf("Digite o codigo: \n");
-    scanf("%s", &codigoDaCIDADE);
+    printf("Digite o codigo: \n", codigoDaCIDADE);
 
-    printf("digite a cidade: \n");
-    scanf("%s", &cidade);
+    printf("digite a cidade: \n", cidade);
 
-    printf("digite o numero de população: \s");
-    scanf("%d", &população);
+    printf("digite o numero de população: \s", população);
 
-    printf("Tamanho da área: \s");
-    scanf("%f", &área);
+    printf("Tamanho da área: \s", área);
 
-    printf("Qual o PIB: \s");
-    scanf("%f",&PIB);
+    printf("Qual o PIB: \s", PIB);
 
-    printf("Numero de pontos turistico: \s");
-    scanf("%d", &numeroPontosturistico);
+    printf("Numero de pontos turistico: \s",numeroPontosturistico);
 
     printf("nome do estado: %s - codigo: %s - cidade:%s", estado, codigoDaCIDADE, cidade);
     printf("população: %d - área: %f - PIB: %f", população, área, PIB);
