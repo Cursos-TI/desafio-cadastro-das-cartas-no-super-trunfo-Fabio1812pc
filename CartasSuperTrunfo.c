46272cfd17fb2b6762e5d5475c9bef6ecc6b9726
Fabio1812pc <fabiolucassilva.fl@gmail.com>
2025-03-25 15:15:54 +0000

    aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,83 +1,73 @@
 #include <stdio.h>
 
-int main () {
-    printf("cidade1! \n");
-    
-    char estado;
-    char codigo[6];
-    char cidade[10];
-    int população;
-    float área;
-    float PIB;
-    int numeropontosturistico;
+   int main() {
+       char Estado[20], Estado2[20];
+       char Cod_carta [5], Cod_carta2[5];
+       char Nome_da_cidade [30], Nome_da_cidade2[30];
+       int Populacao, populacao2;
+       float Area_em_km, Area_em_Km2;
+       float PIB, PIB2;
+       int Tot_Pontos_Turisticos, Tot_Pontos_Turisticos2;
+       float Densidade_Populacional = Populacao / Area_em_km, Densidade_Populacional2 = populacao2 / Area_em_Km2;
+       float pib_per_capita = Populacao / PIB, pib_per_capita2 = populacao2 / PIB2;
+   
+       
+       printf ("Dados da Carta 1.. \n");
+   
+       
+       printf("Digite o nome do Estado: \n");
+       scanf("%s", &Estado);
+       
+       printf("Digite o Código da Carta: \n");
+       scanf("%s", &Cod_carta);
+   
+       printf("Digite o Nome da Cidade: \n");
+       scanf("%s", &Nome_da_cidade);
+   
+       printf("Digite o total da População: \n");
+       scanf("%d", &Populacao);
+   
+       printf("Digite a area de Extensão da Cidade: \n");
+       scanf("%f,", &Area_em_km);
+   
+       printf("PIB: \n");
+       scanf("%f", &PIB);
+   
+       printf("Digite a quantidade de pontos turísticos: \n");
+       scanf("%d", &Tot_Pontos_Turisticos);
 
-    printf("Digite o estado: \n");
-    scanf("%s", &estado);
+       printf("Densidade_Populacional: %f\n, Densidade_Populacional");
 
-    printf("Digite o codigo: \n");
-    scanf("%s", &codigo);
+       printf("pib_per_capita: %f\n, pib_per_capita");
+   
+       printf ("Dados da Carta 2.. \n");
+   
+       
+       printf("Digite o nome do Estado2: \n");
+       scanf("%s", &Estado2);
+       
+       printf("Digite o Código da Carta2: \n");
+       scanf("%s", &Cod_carta2);
+   
+       printf("Digite o Nome da Cidade2: \n");
+       scanf("%s", &Nome_da_cidade2);
+   
+       printf("Digite o total da População2: \n");
+       scanf("%d", &populacao2);
+   
+       printf("Digite a area de Extensão da Cidade2: \n");
+       scanf("%f", &Area_em_Km2);
+   
+       printf("PIB: \n");
+       scanf("%f", &PIB2);
+   
+       printf("Digite a quantidade de pontos turísticos2: \n");
+       scanf("%d", &Tot_Pontos_Turisticos2);
 
-    printf("digite a cidade: \n");
-    scanf("%s", &cidade);
+       printf("Densidade_Populacional2: %f\n, Densidade_Populacional2");
 
-    printf("digite o numero de população: \s");
-    scanf("%d", &população);
-
-    printf("Tamanho da área: \s");
-    scanf("%f", &área);
-
-    printf("Qual o PIB: \s");
-    scanf("%f",&PIB);
-
-    printf("Numero de pontos turistico: \s");
-    scanf("%d", &numeropontosturistico);
-
-    printf("nome do estado: %s - codigo: %s - cidade:%s", estado, codigo, cidade);
-    printf("população: %d - área: %f - PIB: %f", população, área, PIB);
-    printf("numero pontos turistico: %d");
-
-    return 0;
-
-}
-
-#include <stdio.h>
-
-int main () {
-    printf("cidade2! \n");
-    
-    char estado;
-    char codigo[6];
-    char cidade[10];
-    int população;
-    float área;
-    float PIB;
-    int numeropontosturistico;
-
-    printf("Digite o estado: \n");
-    scanf("%s", &estado);
-
-    printf("Digite o codigo: \n");
-    scanf("%s", &codigo);
-
-    printf("digite a cidade: \n");
-    scanf("%s", &cidade);
-
-    printf("digite o numero de população: \s");
-    scanf("%d", &população);
-
-    printf("Tamanho da área: \s");
-    scanf("%f", &área);
-
-    printf("Qual o PIB: \s");
-    scanf("%f",&PIB);
-
-    printf("Numero de pontos turistico: \s");
-    scanf("%d", &numeropontosturistico);
-
-    printf("nome do estado: %s - codigo: %s - cidade:%s", estado, codigo, cidade);
-    printf("população: %d - área: %f - PIB: %f", população, área, PIB);
-    printf("numero pontos turistico: %d");
-
-    return 0;
-
-}
+       printf("pib_per_capita2: %f\n, pib_per_capita2");
+   
+   
+   
+   }
---
+++ numeros/convercao.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int majn(){
+
+    int a = 10;
+    int b = 3;
+    float quociente = (float) a / b;
+
+    printf("Quociente: %2f\n", quociente);
+
+     return 0
+
+     
+}
